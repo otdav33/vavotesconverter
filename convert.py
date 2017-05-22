fded5ffff09809b53a57d30a79b35e3c81cf6295
Owen Davies <odavies@dread.life>
2017-05-22 11:01:39 -0400

    added filename detection

--- convert.py
+++ convert.py
@@ -5,6 +5,14 @@ import sys
 This program will convert a file from historical.elections.virginia.gov to one usable in the database and put in on stdout.
 """
 
+"""Virginia_Elections_Database__2000_President_General_Election.csv"""
+seekstring = "Virginia_Elections_Database__"
+start = sys.argv[1].rindex(seekstring) + len(seekstring)
+data = sys.argv[1][start:]
+ystart = data.index("_")
+year = data[:ystart]
+dindex = data.rindex(".")
+election = data[ystart+1:dindex]
 
 try:
 	inputfile = open(sys.argv[1])
@@ -24,4 +32,4 @@ for i in range(len(l[0]))[3:]: #iterate through candidates
 	c = [name, party]
 	for r in l[2:]: #iterate through counties
 		#add a new output row of candidate name, party, county, votes
-		writer.writerow(c + [r[0], r[i]])
+		writer.writerow(c + [r[0], r[i], year, election])
